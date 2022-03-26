c720189a3e72a9e656c4fcb9af5d736c0ec4d425
ann <asog.jrti@gmail.com>
2022-03-26 13:40:21 +0000

    little change users model

--- src/auth-back/artwork/artwork.controller.js
+++ src/auth-back/artwork/artwork.controller.js
@@ -1,42 +1,5 @@
 import { retrieveArtworkByFileName, retrieveArtworksByUserToken } from "./artwork.model.js";
 
-// const storageEngine = multer.diskStorage({
-//     destination: './public/uploads/',
-//     filename: function (req, file, callback) {
-//         callback(
-//             null,
-//             file.fieldname + '-' + Date.now() + path.extname(file.originalname)
-//         );
-//     },
-// });
-
-// const fileFilter = (req, file, callback) => {
-//     let pattern = /jpg|png|svg/;
-
-//     if (pattern.test(path.extname(file.originalname))) {
-//         callback(null, true);
-//     } else {
-//         callback('Error: not a valid file');
-//     }
-// };
-
-// const upload = multer({
-//     storage: storageEngine,
-//     fileFilter
-// });
-
-// export const uploadArtworkCtrl = () => {
-//     upload.single('uploadedFile'), (req, res) => {
-//         console.log('req file');
-//         console.log(req.file)
-//         res.json(req.file).status(200);
-//     }
-// }
-
-// export const uploadArtwork = (req, res) => {
-
-// }
-
 
 export const getArtworkByFileNameCtrl = async (req, res) => {
     let { filename } = req.params
@@ -44,7 +7,6 @@ export const getArtworkByFileNameCtrl = async (req, res) => {
     if (artwork !== null) res.json(artwork)
     else res.sendStatus(404)
 }
-
 
 export const getUserArtworksCtrl = async (req, res) => {
     console.log('req ctrl', req);
