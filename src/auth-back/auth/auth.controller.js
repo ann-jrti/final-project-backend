6fb5d6043a5d68bed663962099e128b1acfc82ae
ann <asog.jrti@gmail.com>
2022-03-30 23:53:43 +0100

    fix login

--- src/auth-back/auth/auth.controller.js
+++ src/auth-back/auth/auth.controller.js
@@ -1,54 +1,74 @@
-import { createUser, getUserByEmailNoStatus, retrieveSuccessUserByEmailAndPassword, updateValidUser } from "../users/users.model.js";
-import { createValidationToken, deleteValidationToken, retrieveValidationToken } from "./auth.model.js";
-import { generateValidationToken, encodePassword } from "./auth.utils.js";
-import { sendValidationEmail } from "../../adapters/email.js";
+import {
+  createUser,
+  getUserByEmailNoStatus,
+  retrieveSuccessUserByEmailAndPassword,
+  updateValidUser,
+  retrieveDataCustomLolProfile,
+} from '../users/users.model.js';
+import {
+  createValidationToken,
+  deleteValidationToken,
+  retrieveValidationToken,
+} from './auth.model.js';
+import { generateValidationToken, encodePassword } from './auth.utils.js';
+import { sendValidationEmail } from '../../adapters/email.js';
 import jwt from 'jsonwebtoken';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
 const { DB_PW, JWT_SECRET } = process.env;
 
 export const registerCtrl = async (req, res) => {
-    try {
-        const user = await getUserByEmailNoStatus(req.body.email);
-        if (user === null) {
-            req.body.password = encodePassword(req.body.password);
-            await createUser({ ...req.body, status: 'pending-validation' }); // creates user and saves it in database
-            const token = generateValidationToken(); // generates token for user
-            await createValidationToken(token, req.body.email); // saves it in db and ties token to user
-            sendValidationEmail(req.body.email, `http://localhost:3000/validate?token=${token}`);
-            res.sendStatus(201);
-        } else {
-            res.sendStatus(409) //it already exists the user in the ddbb
-        }
-
-    } catch (err) {
-        log.info(err);
-        res.sendStatus(500);
+  try {
+    const user = await getUserByEmailNoStatus(req.body.email);
+    if (user === null) {
+      req.body.password = encodePassword(req.body.password);
+      await createUser({ ...req.body, status: 'pending-validation' }); // creates user and saves it in database
+      const token = generateValidationToken(); // generates token for user
+      await createValidationToken(token, req.body.email); // saves it in db and ties token to user
+      sendValidationEmail(
+        req.body.email,
+        `http://localhost:3000/validate?token=${token}`
+      );
+      res.sendStatus(201);
+    } else {
+      res.sendStatus(409); //it already exists the user in the ddbb
     }
-}
+  } catch (err) {
+    log.info(err);
+    res.sendStatus(500);
+  }
+};
 
 export const validateEmailCtrl = async (req, res) => {
-    const { token } = req.query;
-    log.info(token);
-    const validateToken = await retrieveValidationToken(token);
-    if (validateToken !== null) {
-        await deleteValidationToken(token);
-        await updateValidUser(validateToken.user);
-        res.send(200);
-    } else {
-        res.sendStatus(404);
-    }
-}
+  const { token } = req.query;
+  log.info(token);
+  const validateToken = await retrieveValidationToken(token);
+  if (validateToken !== null) {
+    await deleteValidationToken(token);
+    await updateValidUser(validateToken.user);
+    res.send(200);
+  } else {
+    res.sendStatus(404);
+  }
+};
 
 export const loginCtrl = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await retrieveSuccessUserByEmailAndPassword(email, encodePassword(password));
-    if (user !== null) {
-        const token = jwt.sign({ email: user.email }, JWT_SECRET);
-        res.status(201).json({ access_token: token, email: user.email, username: user.username });
-    } else {
-        res.sendStatus(404);
-    }
-
-}
+  const { email, password } = req.body;
+  const user = await retrieveSuccessUserByEmailAndPassword(
+    email,
+    encodePassword(password)
+  );
+  const customProfile = await retrieveDataCustomLolProfile(email);
+  if (user !== null) {
+    const token = jwt.sign({ email: user.email }, JWT_SECRET);
+    res.status(201).json({
+      access_token: token,
+      email: user.email,
+      username: user.username,
+      playername: customProfile.basicInfo.name,
+    });
+  } else {
+    res.sendStatus(404);
+  }
+};
--- src/auth-back/users/users.model.js
+++ src/auth-back/users/users.model.js
@@ -1,170 +1,171 @@
-import { db } from "../../database/index.js"
-import { getPlayerOfferByPlayerEmail } from "../players-pool/players-pool.model.js";
+import { db } from '../../database/index.js';
+import { getPlayerOfferByPlayerEmail } from '../players-pool/players-pool.model.js';
 const USERS_COLLECTION = 'users';
 const CUSTOM_PROFILES_COLLECTION = 'users-custom-profiles';
 const PLAYERS_POOL_COLLECTION = 'players-pool';
 const ARTWORK_COLLECTION = 'users-artworks';
 
-
 export const createUser = async (user) => {
-    try {
-        const users = db.collection(USERS_COLLECTION);
-        return await users.insertOne(user);
-    } catch (err) {
-        log.info(err);
-    }
-}
+  try {
+    const users = db.collection(USERS_COLLECTION);
+    return await users.insertOne(user);
+  } catch (err) {
+    log.info(err);
+  }
+};
 
 // returns user if exists
 export const getUserByEmailNoStatus = async (email) => {
-    try {
-        const users = db.collection(USERS_COLLECTION);
-        return await users.findOne({ email });
-    } catch (err) {
-        log.error(err);
-    }
-}
+  try {
+    const users = db.collection(USERS_COLLECTION);
+    return await users.findOne({ email });
+  } catch (err) {
+    log.error(err);
+  }
+};
 
 // updates user data changing status to success
 export const updateValidUser = async (email) => {
-    try {
-        const users = db.collection(USERS_COLLECTION);
-        const updateDoc = {
-            $set: {
-                status: 'SUCCESS'
-            }
-        };
-        return await users.updateOne({ email }, updateDoc);
-    } catch (err) {
-        log.error(err);
-    }
-}
+  try {
+    const users = db.collection(USERS_COLLECTION);
+    const updateDoc = {
+      $set: {
+        status: 'SUCCESS',
+      },
+    };
+    return await users.updateOne({ email }, updateDoc);
+  } catch (err) {
+    log.error(err);
+  }
+};
 
 // returns user in ddbb with success status if email and password matches
-export const retrieveSuccessUserByEmailAndPassword = async (email, password) => {
-    try {
-        const users = db.collection(USERS_COLLECTION);
-        const query = {
-            email,
-            password,
-            status: 'SUCCESS'
-        }
-        return await users.findOne(query);
-    } catch (err) {
-        log.error(err);
-    }
-}
+export const retrieveSuccessUserByEmailAndPassword = async (
+  email,
+  password
+) => {
+  try {
+    const users = db.collection(USERS_COLLECTION);
+    const query = {
+      email,
+      password,
+      status: 'SUCCESS',
+    };
+
+    return await users.findOne(query);
+  } catch (err) {
+    log.error(err);
+  }
+};
 export const retrieveUserInfoByEmail = async (email) => {
-    try {
-        const users = db.collection(USERS_COLLECTION);
-        const query = { email };
-        return await users.findOne(query);
-    } catch (err) {
-        log.error(err);
-    }
-}
+  try {
+    const users = db.collection(USERS_COLLECTION);
+    const query = { email };
+    return await users.findOne(query);
+  } catch (err) {
+    log.error(err);
+  }
+};
 
 export const createLolProfile = async (stats, email) => {
-    try {
-        const customProfiles = db.collection(CUSTOM_PROFILES_COLLECTION);
-        const query = { email };
-        const isThereCustomProfile = await customProfiles.findOne(query)
-        if (isThereCustomProfile === null) {
-            return await customProfiles.insertOne(stats);
-        } else {
-            const newStats = stats;
-            return await customProfiles.replaceOne(isThereCustomProfile, newStats);
-        }
-
-    } catch (err) {
-        log.error('Create lol profile error: ', err);
+  try {
+    const customProfiles = db.collection(CUSTOM_PROFILES_COLLECTION);
+    const query = { email };
+    const isThereCustomProfile = await customProfiles.findOne(query);
+    if (isThereCustomProfile === null) {
+      return await customProfiles.insertOne(stats);
+    } else {
+      const newStats = stats;
+      return await customProfiles.replaceOne(isThereCustomProfile, newStats);
     }
-}
+  } catch (err) {
+    log.error('Create lol profile error: ', err);
+  }
+};
 
 export const retrieveCustomLolProfile = async (email) => {
-    try {
-        const customLolProfile = db.collection(CUSTOM_PROFILES_COLLECTION);
-        const query = { email };
-        // const options = { projection: { _id: 0, password: 0, status: 0 } }
-        return await customLolProfile.findOne(query);
-    } catch (err) {
-        log.error(err);
-    }
-}
+  try {
+    const customLolProfile = db.collection(CUSTOM_PROFILES_COLLECTION);
+    const query = { email };
+    // const options = { projection: { _id: 0, password: 0, status: 0 } }
+    return await customLolProfile.findOne(query);
+  } catch (err) {
+    log.error(err);
+  }
+};
 
 export const updateCustomProfileStatus = async (email) => {
-    try {
-        const users = db.collection(USERS_COLLECTION);
-        const updateDoc = {
-            $set: {
-                customProfile: true
-            }
-        };
-        return await users.updateOne({ email }, updateDoc);
-    } catch (err) {
-        log.error(err);
-    }
-}
+  try {
+    const users = db.collection(USERS_COLLECTION);
+    const updateDoc = {
+      $set: {
+        customProfile: true,
+      },
+    };
+    return await users.updateOne({ email }, updateDoc);
+  } catch (err) {
+    log.error(err);
+  }
+};
 
 export const retrieveDataCustomLolProfile = async (email) => {
-    try {
-        const customLolProfile = db.collection(CUSTOM_PROFILES_COLLECTION);
-        const query = { email };
-        // const options = { projection: { _id: 0, password: 0, status: 0 } }
-        return await customLolProfile.findOne(query);
-    } catch (err) {
-        log.error(err);
-    }
-}
+  try {
+    const customLolProfile = db.collection(CUSTOM_PROFILES_COLLECTION);
+    const query = { email };
+    // const options = { projection: { _id: 0, password: 0, status: 0 } }
+    return await customLolProfile.findOne(query);
+  } catch (err) {
+    log.error(err);
+  }
+};
 
 export const deleteUserAccount = async (email) => {
-    console.log('on delete user account')
-    try {
-        const users = db.collection(USERS_COLLECTION);
-        const query = { email };
-        // await customProfiles.findOne(query)
-        await users.deleteOne(query);
-
-    } catch (err) {
-        log.error('Create lol profile error: ', err);
-    }
-}
+  console.log('on delete user account');
+  try {
+    const users = db.collection(USERS_COLLECTION);
+    const query = { email };
+    // await customProfiles.findOne(query)
+    await users.deleteOne(query);
+  } catch (err) {
+    log.error('Create lol profile error: ', err);
+  }
+};
 
 export const deleteCustomProfile = async (email) => {
-    console.log('on delete custom profile');
-    try {
-        const customProfiles = db.collection(CUSTOM_PROFILES_COLLECTION);
-        const query = { email };
-        console.log('query', query)
+  console.log('on delete custom profile');
+  try {
+    const customProfiles = db.collection(CUSTOM_PROFILES_COLLECTION);
+    const query = { email };
+    console.log('query', query);
 
-        await customProfiles.deleteOne(query);
-    } catch (err) {
-        log.error('Create lol profile error: ', err);
-    }
-}
+    await customProfiles.deleteOne(query);
+  } catch (err) {
+    log.error('Create lol profile error: ', err);
+  }
+};
 
 export const deleteActiveOffer = async (email) => {
-    try {
-        console.log('on delete active offer');
-        const playerOffers = db.collection(PLAYERS_POOL_COLLECTION);
-        const query = { email };
-        console.log('query', query)
-        // await customProfiles.findOne(query)
-        await playerOffers.deleteOne(query);
-    } catch (err) {
-        log.error('Create lol profile error: ', err);
-    }
-}
+  try {
+    console.log('on delete active offer');
+    const playerOffers = db.collection(PLAYERS_POOL_COLLECTION);
+    const query = { email };
+    console.log('query', query);
+    // await customProfiles.findOne(query)
+    await playerOffers.deleteOne(query);
+  } catch (err) {
+    log.error('Create lol profile error: ', err);
+  }
+};
 
 export const deleteUserArtworks = async (email) => {
-    try {
-        console.log('on delete artworks');
-        const artworks = db.collection(ARTWORK_COLLECTION);
-        const query = { email };
-        console.log('query', query)
-        // await customProfiles.findOne(query)
-        await artworks.deleteOne(query);
-    } catch (err) {
-        log.error('Create lol profile error: ', err);
-    }
-}
+  try {
+    console.log('on delete artworks');
+    const artworks = db.collection(ARTWORK_COLLECTION);
+    const query = { email };
+    console.log('query', query);
+    // await customProfiles.findOne(query)
+    await artworks.deleteOne(query);
+  } catch (err) {
+    log.error('Create lol profile error: ', err);
+  }
+};
