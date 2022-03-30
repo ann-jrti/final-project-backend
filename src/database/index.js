73a040a731a9c1117df4d9318e94eb42dfbf0cfd
ann <asog.jrti@gmail.com>
2022-03-30 15:15:58 +0100

    heroku

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,6 @@ const { DB_PW } = process.env;
 const DATABASE_NAME = 'ann-final-project';
 const URI = `mongodb+srv://andrea:${DB_PW}@cluster0.s9dbl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-
 export let db = () => {
     log.error('Database connection not available yet')
 }
@@ -17,3 +16,5 @@ export const connectDB = async () => {
     db = client.db(DATABASE_NAME);
     log.info(`Conected to db ${DATABASE_NAME}`)
 }
+
+console.log(DB_PW)
