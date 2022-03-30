0bb8f96754aa43dc3f74dffe2537b8f4be58fc02
ann <asog.jrti@gmail.com>
2022-03-30 19:52:13 +0100

    Handle DB connection throw

--- src/database/index.js
+++ src/database/index.js
@@ -8,13 +8,18 @@ export let db = () => {
 }
 
 export const connectDB = async () => {
-    log.info(`Connecting to mongodbs ${URI}`)
-    const client = new MongoClient(URI);
-    const conenctionTime = log.startTimer();
-    await client.connect();
-    conenctionTime.done({ message: 'Connected to mongo' });
-    db = client.db(DATABASE_NAME);
-    log.info(`Conected to db ${DATABASE_NAME}`)
+    try {
+        log.info(`Connecting to mongodbs ${URI}`)
+        const client = new MongoClient(URI);
+        const conenctionTime = log.startTimer();
+        await client.connect();
+        conenctionTime.done({ message: 'Connected to mongo' });
+        db = client.db(DATABASE_NAME);
+        log.info(`Conected to db ${DATABASE_NAME}`)
+    } catch (e) {
+        log.error(e)
+    }
+
 }
 
 console.log(DB_PW)
